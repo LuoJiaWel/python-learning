a3c533ecd641c1d4abf6711326db64facd16f3e6
LuoJiaWel <tayloraydin@gmail.com>
2026-07-28 08:18:34 +0800

    Learn input, output and f-string

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello, Python!")
+name = input("請輸入姓名")
+
+income = int(input("請輸入今天收入："))
+
+expense = int(input("請輸入今天支出："))
+
+balance = income - expense
+
+print(f"{name}，你今天剩餘 {balance} 元")
