ea775db5d17900103350f0d0e16d0cc3b511a978
LuoJiaWel <tayloraydin@gmail.com>
2026-07-29 14:25:40 +0800

    if 判斷式

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ expense = int(input("請輸入今天支出："))
 
 balance = income - expense
 
-print(f"{name}，你今天剩餘 {balance} 元")
+if balance >= 0:
+    print(f"{name}，你今天剩餘 {balance} 元")
+elif balance == 0:
+    print(f"{name}，你今天剛好打平")
+else:
+    print(f"{name}，今天超支{abs(balance)}")
