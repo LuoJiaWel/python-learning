071c911d5afa854ddea9d9895eac7b34469ae215
LuoJiaWel <tayloraydin@gmail.com>
2026-07-30 16:13:06 +0800

    Dictionary and List

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-name = input("請輸入姓名")
+record = {
+    "date": "2026/07/30",
+    "category": "game",
+    "expense": 1500,
+    "note": "遊戲"
+}
 
-income = int(input("請輸入今天收入："))
-
-expense = int(input("請輸入今天支出："))
-
-balance = income - expense
-
-if balance >= 0:
-    print(f"{name}，你今天剩餘 {balance} 元")
-elif balance == 0:
-    print(f"{name}，你今天剛好打平")
-else:
-    print(f"{name}，今天超支{abs(balance)}")
+print(record["date"])
+print(record["note"])
