4a888c324c81fd26b0970150b0f2d99af2300234
LuoJiaWel <tayloraydin@gmail.com>
2026-08-03 14:38:45 +0800

    total = 0 初始化 total = total + something 累加 for 逐筆處理資料 record["expense"]取得Dictionary資料 何必處理流程，避免重複迴圈

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-record = {
-    "date": "2026/07/30",
-    "category": "game",
-    "expense": 1500,
-    "note": "遊戲"
-}
+records = []
 
-print(record["date"])
-print(record["note"])
+for i in range(2):
+
+    print(f"\n=== 第 {i + 1} 筆記帳 ===")
+
+    # 請輸入日期
+    date = input("請輸入日期：")
+    # 請輸入類別
+    category = input("請輸入類別：")
+    # 請輸入支出
+    expense = int(input("請輸入支出："))
+    # 請輸入備註
+    remark = input("請輸入備註：")
+
+    record = {
+        "date" : date,
+        "category" : category,
+        "expense" : expense,
+        "remark" : remark
+    }
+    records.append(record)
+
+total = 0
+
+print("\n所有記帳資料：")
+
+for record in records:
+    print(record)
+    total = total + record["expense"]
+
+print(f"\n總支出：{total}元")
