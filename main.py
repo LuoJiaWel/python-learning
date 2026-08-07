1c4511d91568ebbbaadfbd5abdaa25bfe66fc721
LuoJiaWel <tayloraydin@gmail.com>
2026-08-07 14:32:17 +0800

    Money Tracker v0.2 完成

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ for i in range(2):
 
     print(f"\n=== 第 {i + 1} 筆記帳 ===")
 
-    # 請輸入日期
     date = input("請輸入日期：")
-    # 請輸入類別
     category = input("請輸入類別：")
-    # 請輸入支出
     expense = int(input("請輸入支出："))
-    # 請輸入備註
     remark = input("請輸入備註：")
 
     record = {
@@ -26,7 +22,13 @@ total = 0
 print("\n所有記帳資料：")
 
 for record in records:
-    print(record)
-    total = total + record["expense"]
+    print(f"日期：{record['date']}")
+    print(f"類別：{record['category']}")
+    print(f"支出：{record['expense']}")
+    print(f"備註：{record['remark']}")
+
+    total = total + record['expense']
+
+    print()
 
 print(f"\n總支出：{total}元")
