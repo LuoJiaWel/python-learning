21854636a714d2c95e760a7b924f38113e249d26
LuoJiaWel <tayloraydin@gmail.com>
2026-08-10 13:08:19 +0800

    Function 的分工與責任

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 records = []
 
-for i in range(2):
-
+def input_record():
     print(f"\n=== 第 {i + 1} 筆記帳 ===")
 
     date = input("請輸入日期：")
@@ -10,25 +9,24 @@ for i in range(2):
     remark = input("請輸入備註：")
 
     record = {
-        "date" : date,
-        "category" : category,
-        "expense" : expense,
-        "remark" : remark
+        "date": date,
+        "category": category,
+        "expense": expense,
+        "remark": remark
     }
     records.append(record)
 
-total = 0
+def show_records():
+    total = 0
+    print("\n所有記帳資料：")
 
-print("\n所有記帳資料：")
+    for record in records:
+        print(record)
+        total = total + record["expense"]
 
-for record in records:
-    print(f"日期：{record['date']}")
-    print(f"類別：{record['category']}")
-    print(f"支出：{record['expense']}")
-    print(f"備註：{record['remark']}")
+    print(f"\n總支出：{total}元")
 
-    total = total + record['expense']
 
-    print()
-
-print(f"\n總支出：{total}元")
+for i in range(2):
+    input_record()
+    show_records()
